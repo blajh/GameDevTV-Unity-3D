bd01ca631dd38b4c86a4b96335a97fdcb2779868
Andrej Kovacevic <glazba@gmail.com>
2023-02-18 00:18:30 +0100

    Bool Variable For State

--- GDTV - 3D - Project Boost/Assets/Scripts/CollisionHandler.cs
+++ GDTV - 3D - Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -12,39 +12,44 @@ public class CollisionHandler : MonoBehaviour
     [SerializeField] private AudioClip successSFX;
 
     private AudioSource audioSource;
+    private bool isTransitioning = false;
 
     private void Awake() {
         audioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision) {
-        string tag = collision.gameObject.tag;
-        switch (tag) {
-            case "Friendly":
-                Debug.Log("A friendly bump.");
-                break;
-            case "Fuel":
-                Debug.Log("Fuel is pumping engines.");
-                Destroy(collision.gameObject);
-                break;
-            case "Finish":
-                Debug.Log("You've made it");
-                StartSuccessSequence();                
-                break;
-            default:
-                Debug.Log("Boom goes the rocket");
-                StartCrashSequence();
-                break;
+
+        if(!isTransitioning) {
+            string tag = collision.gameObject.tag;
+            switch (tag) {
+                case "Friendly":
+                    Debug.Log("A friendly bump.");
+                    break;
+                case "Finish":
+                    Debug.Log("You've made it");
+                    StartSuccessSequence();                
+                    break;
+                default:
+                    Debug.Log("Boom goes the rocket");
+                    StartCrashSequence();
+                    break;
+            }
         }
     }
 
     private void StartSuccessSequence() {
+        isTransitioning = true;
+        audioSource.Stop();
         audioSource.PlayOneShot(successSFX);
+        // TODO add Particle System
         GetComponent<Movement>().enabled = false;        
         Invoke(LOAD_NEXT_LEVEL, loadDelay);
     }
 
     private void StartCrashSequence() {
+        isTransitioning = true;
+        audioSource.Stop();
         audioSource.PlayOneShot(crashSFX, .1f);
         // TODO add Particle System
         GetComponent<Movement>().enabled = false;        
