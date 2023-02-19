419cfd62e169d8f539b8407aa5aa394c430bb648
Andrej Kovacevic <glazba@gmail.com>
2023-02-19 21:55:31 +0100

    Start Moving Our Player

--- GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
+++ GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 20f;
+
+
     private void Update() {
         float xThrow = Input.GetAxis("Horizontal");        
-        float yThrow = Input.GetAxis("Vertical");             
+        float yThrow = Input.GetAxis("Vertical");
+
+        transform.localPosition = new Vector3(transform.localPosition.x + xThrow * moveSpeed * Time.deltaTime,
+                                              transform.localPosition.y + yThrow * moveSpeed * Time.deltaTime,
+                                              transform.localPosition.z);
+
     }
 }
