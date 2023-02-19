c10eedf775b313e2c10ffa20ac6d5b670d915c9c
Andrej Kovacevic <glazba@gmail.com>
2023-02-19 21:47:36 +0100

    package tidy

--- GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
+++ GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,29 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
-
-    private void OnEnable() {
-        movement.Enable();
-    }
-
-    private void OnDisable() {
-        movement.Disable();
-    }
-
     private void Update() {
-
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-
-        //float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
-        
-        //float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+        float xThrow = Input.GetAxis("Horizontal");        
+        float yThrow = Input.GetAxis("Vertical");             
     }
 }
