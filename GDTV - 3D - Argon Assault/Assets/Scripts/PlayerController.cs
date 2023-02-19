5bfbed32881737c4bbfe100f05f8ba9fa4dc17ce
Andrej Kovacevic <glazba@gmail.com>
2023-02-19 22:06:52 +0100

    Mathf.Clamp() To Constrain Movement

--- GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
+++ GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 30f;
+    [SerializeField] private float xClampRange = 10f;
+    [SerializeField] private float yClampRange = 5f;
+    [SerializeField] private float yClampCenter = 2f;
 
 
     private void Update() {
         float xThrow = Input.GetAxis("Horizontal");        
         float yThrow = Input.GetAxis("Vertical");
 
-        transform.localPosition = new Vector3(transform.localPosition.x + xThrow * moveSpeed * Time.deltaTime,
-                                              transform.localPosition.y + yThrow * moveSpeed * Time.deltaTime,
-                                              transform.localPosition.z);
+        float xOffset = xThrow * moveSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+        float yOffset = yThrow * moveSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, yClampCenter - yClampRange, yClampCenter + yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
 
     }
 }
