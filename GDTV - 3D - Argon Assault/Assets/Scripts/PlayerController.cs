2de0de0858c0430e05972dd1beea44b164b115a0
Andrej Kovacevic <glazba@gmail.com>
2023-02-19 21:36:30 +0100

    New Unity Input System

--- GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
+++ GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+
+    private void OnEnable() {
+        movement.Enable();
+    }
+
+    private void OnDisable() {
+        movement.Disable();
+    }
+
     private void Update() {
-        
-        float verticalThrow = Input.GetAxis("Vertical");
+
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+        //float verticalThrow = Input.GetAxis("Vertical");
         Debug.Log(verticalThrow);
         
-        float horizontalThrow = Input.GetAxis("Horizontal");
+        //float horizontalThrow = Input.GetAxis("Horizontal");
         Debug.Log(horizontalThrow);
     }
 }
