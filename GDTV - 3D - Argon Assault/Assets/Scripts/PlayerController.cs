b699409d3e9baed1cdf2668c0f84caf46fcf7d92
Andrej Kovacevic <glazba@gmail.com>
2023-02-19 22:41:12 +0100

    Rotate Ship With Position & Throw

--- GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
+++ GDTV - 3D - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,18 +12,48 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float yClampRange = 5f;
     [SerializeField] private float yClampCenter = 2f;
 
+    [Header("Rotations")]
+    [SerializeField] private float positionPitchFactor = -2f;
+    [SerializeField] private float controlPitchFactor = -10f;
+    [SerializeField] private float positionYawFactor = 2f;
+    [SerializeField] private float controlRollFactor = -20f;
+    [SerializeField] private bool affectPitch = true;
+    [SerializeField] private bool affectYaw = true;
+    [SerializeField] private bool affectRoll = true;
+
+    private float xThrow, yThrow;
+
     private void Update() {
         ProcessTranslation();
         ProcessRotation();
     }
 
     private void ProcessRotation() {
-        transform.localRotation = Quaternion.Euler(-30f, 30f, 0);
+
+        float pitchDueToPosition = (transform.localPosition.y - yClampCenter) * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+                
+        float roll = xThrow * controlRollFactor;
+
+        if (affectPitch) {
+            transform.localRotation = Quaternion.Euler(pitch, transform.localRotation.y, transform.localRotation.z);
+        }
+
+        if (affectYaw) {
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x, yaw, transform.localRotation.z);
+        }
+
+        if (affectRoll) {
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, roll);
+        }
     }
 
     private void ProcessTranslation() {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * moveSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
