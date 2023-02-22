11d39b66d1b8ccd7cdd020c051a004347932c546
Andrej Kovacevic <glazba@gmail.com>
2023-02-22 15:43:49 +0100

    Simple User Interface For Score

--- GDTV - 3D - Argon Assault/Assets/Scripts/ScoreBoard.cs
+++ GDTV - 3D - Argon Assault/Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI scoreText;
+
     private int score = 0;
+
+    private void Start() {
+        DisplayScore();
+    }
 
     public void AddToScore(int amount) {
         score += amount;
+        DisplayScore();
+    }
+
+    private void DisplayScore() {
+        scoreText.text = "SCORE: " + score.ToString();
     }
 
     public int GetScore() {
