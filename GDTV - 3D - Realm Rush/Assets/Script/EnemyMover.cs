da9c7b19edbc12f02f140cd5d8520a9611f45428
Andrej Kovacevic <glazba@gmail.com>
2023-02-24 10:36:26 +0100

    Smooth Enemy Movement & Rotation

--- GDTV - 3D - Realm Rush/Assets/Script/EnemyMover.cs
+++ GDTV - 3D - Realm Rush/Assets/Script/EnemyMover.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
 	[SerializeField] private List<Waypoint> path = new List<Waypoint>();
-	[SerializeField] private float waitTime = 1f;
+	[SerializeField] [Range(0f, 5f)] private float moveSpeed = 1f;
+	[SerializeField] [Range(0f, 5f)] private float rotationDuration = 1f;
 
-	private void Start() {
+    private void Start() {
 		StartCoroutine(FollowPath());
 	}
 
 	private IEnumerator FollowPath() {
 		foreach (Waypoint waypoint in path) {
-			transform.position = waypoint.transform.position;
-			yield return new WaitForSeconds(waitTime);
+
+			float rotationTime = 0f;
+			Vector3 targetDir = waypoint.transform.position - transform.position;
+
+            Quaternion newRot = Quaternion.LookRotation(targetDir, Vector3.up);
+
+			while (transform.rotation != newRot) {
+				rotationTime += Time.deltaTime;
+				transform.rotation = Quaternion.Lerp(transform.rotation, newRot, rotationTime / rotationDuration);
+				yield return null;
+			}
+
+			if (transform.rotation == newRot) {
+
+                Vector3 startPos = transform.position;
+                Vector3 endPos = waypoint.transform.position;
+                float travelPercent = 0f;
+
+				while (travelPercent < 1f) {
+					travelPercent += Time.deltaTime * moveSpeed;
+					transform.position = Vector3.Lerp(startPos, endPos, travelPercent);			
+					Debug.Log(travelPercent.ToString());
+					Debug.Log(waypoint.name);
+					Debug.Log(Time.deltaTime);
+					yield return new WaitForEndOfFrame();
+				}
+            }
 		}
 	}
 }
