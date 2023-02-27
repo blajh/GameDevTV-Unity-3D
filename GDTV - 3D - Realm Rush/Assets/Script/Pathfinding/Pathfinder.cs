85deb358f52632767b93ded57c51cb6fe2c79dcd
Andrej Kovacevic <glazba@gmail.com>
2023-02-27 12:07:03 +0100

    Exploring the World

--- GDTV - 3D - Realm Rush/Assets/Script/Pathfinding/Pathfinder.cs
+++ GDTV - 3D - Realm Rush/Assets/Script/Pathfinding/Pathfinder.cs
@@ -5,32 +5,64 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    [SerializeField] private Node currentSearchNode;
+    [SerializeField] private Vector2Int startCoordinates;
+    [SerializeField] private Vector2Int destinationCoordinates;
+
+    private Node startNode;
+    private Node destinationNode;
+    private Node currentSearchNode;
+
+    private Queue<Node> frontier = new Queue<Node>();
+    private Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();
+
     private Vector2Int[] directions = {Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down};
     private GridManager gridManager;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
     private void Awake() {
         gridManager = FindObjectOfType<GridManager>();
+        if (gridManager != null ) {
+            grid = gridManager.Grid;
+        }
+        startNode = new Node(startCoordinates, true);
+        destinationNode = new Node(destinationCoordinates, true);
     }
 
     private void Start() {
-        ExploreNeighbors();
+        BreadthFirstSearch();
     }
 
     private void ExploreNeighbors() {
         List<Node> neighbors = new List<Node>();
 
         foreach (Vector2Int direction in directions) {
-            Vector2Int neighborCoordinates = currentSearchNode.coordinates + direction;            
-            if (gridManager == null) { return; }
-            Node neighbor = gridManager.GetNode(neighborCoordinates);            
-            if (neighbor == null) { return; }            
-            neighbors.Add(neighbor);
+            Vector2Int neighborCoordinates = currentSearchNode.coordinates + direction;                        
+            if (grid.ContainsKey(neighborCoordinates)) {
+                neighbors.Add(grid[neighborCoordinates]);
+            }
+        }
 
-            // TODO remove after testing            
-            Dictionary<Vector2Int, Node> grid = gridManager.Grid;
-            grid[neighborCoordinates].isExplored = true;
-            grid[currentSearchNode.coordinates].isPath = true;
+        foreach (Node neighbor in neighbors) {
+            if(!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable) {
+                reached.Add(neighbor.coordinates, neighbor);
+                frontier.Enqueue(neighbor);
+            }
+        }
+    }
+
+    void BreadthFirstSearch() {
+        bool isRunning = true;
+
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+
+        while (frontier.Count > 0 && isRunning) {
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if (currentSearchNode.coordinates == destinationCoordinates) {
+                isRunning = false;
+            }
         }
     }
 }
