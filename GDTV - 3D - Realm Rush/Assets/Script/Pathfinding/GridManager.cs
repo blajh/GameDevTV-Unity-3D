628bfd933a662c4cc51940815949329062b369c8
Andrej Kovacevic <glazba@gmail.com>
2023-02-27 10:52:34 +0100

    Dictionaries

--- GDTV - 3D - Realm Rush/Assets/Script/Pathfinding/GridManager.cs
+++ GDTV - 3D - Realm Rush/Assets/Script/Pathfinding/GridManager.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private Node node;
+    [SerializeField] private Vector2Int gridSize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
-    private void Start() {
-        Debug.Log(node.coordinates);
-        Debug.Log(node.isWalkable);
+    private void Awake() {
+        CreateGrid();
+    }
+
+    private void CreateGrid() {
+        for (int x = 0; x < gridSize.x; x++) {
+            for (int y = 0;  y < gridSize.y; y++) {
+                Vector2Int coordinates = new Vector2Int(x, y);
+                grid.Add(coordinates, new Node(coordinates, true));
+                Debug.Log(grid[coordinates].coordinates + " = " + grid[coordinates].isWalkable);
+            }
+        }
     }
 }
