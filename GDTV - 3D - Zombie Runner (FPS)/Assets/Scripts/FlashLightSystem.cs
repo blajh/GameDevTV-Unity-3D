43b28cc7ead9e633fb6287cd8441e7ea11c83969
Andrej Kovacevic <glazba@gmail.com>
2023-03-07 22:58:19 +0100

    Create A FlashLight

---
+++ GDTV - 3D - Zombie Runner (FPS)/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] private float lightIntensityDecay = 0.1f;
+    [SerializeField] private float lightAngleDecay = 1f;
+    [SerializeField] private float minimumAngle = 20f;
+
+    private Light myLight;
+
+    private void Awake() {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update() {
+        DecreaseLightIntensity();
+        DecreaseLightAngle();
+    }
+
+    private void DecreaseLightAngle() {
+        if (myLight.spotAngle <= minimumAngle) {
+            return;
+        }
+        myLight.spotAngle -= lightAngleDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightIntensity() {
+        myLight.intensity -= lightIntensityDecay * Time.deltaTime;
+    }
+}
