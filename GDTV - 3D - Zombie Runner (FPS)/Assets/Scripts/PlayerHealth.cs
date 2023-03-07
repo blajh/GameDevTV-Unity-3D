4d5a9f4a2768ba227e5e70f2842c36526cd5d01f
Andrej Kovacevic <glazba@gmail.com>
2023-03-07 23:56:46 +0100

    Damage Received UI

--- GDTV - 3D - Zombie Runner (FPS)/Assets/Scripts/PlayerHealth.cs
+++ GDTV - 3D - Zombie Runner (FPS)/Assets/Scripts/PlayerHealth.cs
@@ -1,22 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     // [SerializeField] private int maxHealth = 200;
     [SerializeField] private int startHealth = 100;
     [SerializeField] private int currentHealth;
+    [SerializeField] private Image bloodImage;
+
+    private Color transparentWhite = new Color(255f, 255f, 255f, 0);
+    private float timer = 0f;
 
     private void Start() {
         currentHealth = startHealth;
     }
 
+    private void Update() {
+        if (bloodImage.color.a > 0) {
+            timer += Time.deltaTime;
+            bloodImage.color = Color.Lerp(bloodImage.color, transparentWhite, timer);
+        } else {
+            timer = 0f;
+        }
+    }
+
     public void Damage(int damage) {
+        BloodUI();
         currentHealth -= damage;        
         if(currentHealth < 0) {
             Debug.Log("Player Died");
             GetComponent<DeathHandler>().HandleDeath();
         }
-    }        
+    }
+
+    private void BloodUI() {
+        Vector3 randomZ = new Vector3(0f, 0f, UnityEngine.Random.Range(0f, 360f));
+        bloodImage.rectTransform.rotation = Quaternion.Euler(randomZ);
+        bloodImage.color = Color.white;
+    }
 }
